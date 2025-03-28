ba82742763b3d309b54a176020489dac299d95f2
Vo Thi Mai Hoa <vothimaihoa123@gmail.com>
2025-03-29 02:24:06 +0700

    Added statistics for admin

--- src/features/dashboard/APIs/index.ts
+++ src/features/dashboard/APIs/index.ts
@@ -8,10 +8,11 @@ export type StatisticsQueryParams = {
     StartDate?: string,
     EndDate?: string,
     Top?: number,
+    GroupBy?: string,
 }
 
 export const getOverviewStatistics = async (searchParams: StatisticsQueryParams) => {
-    return get(`${statisticsEndpoint}/count-overview-data`, searchParams)
+    return get(`${statisticsEndpoint}/count-overview-school-data`, searchParams)
 }
 
 export const getRecentTests = async (searchParams: StatisticsQueryParams) => {
@@ -24,4 +25,25 @@ export const getAppointmentStatistics = async (searchParams: StatisticsQueryPara
 
 export const getSupportingProgramStatistics = async (searchParams: StatisticsQueryParams) => {
     return get(`${statisticsEndpoint}/supporting-programs`, searchParams)
+}
+
+// admin dashboard apis
+export const getOverviewSystemStatistics = async (searchParams: StatisticsQueryParams) => {
+    return get(`${statisticsEndpoint}/overview-system-data`, searchParams)
+}
+
+export const getRevenueTimeStatistics = async (searchParams: StatisticsQueryParams) => {
+    return get(`${statisticsEndpoint}/revenue-time-analysis`, searchParams)
+}
+
+export const getTopPsychologistsRevenue = async (searchParams: StatisticsQueryParams) => {
+    return get(`${statisticsEndpoint}/top-psychologists`, searchParams)
+}
+
+export const getRevenueBySpecStatistics = async (searchParams: StatisticsQueryParams) => {
+    return get(`${statisticsEndpoint}/revenue-spec-analysis`, searchParams)
+}
+
+export const getTopSchools = async (searchParams: StatisticsQueryParams) => {
+    return get(`${statisticsEndpoint}/top-schools`, searchParams)
 }
